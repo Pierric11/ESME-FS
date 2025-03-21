1ff535ed743751b6dedb2f86a8b24c1b71444c38
Pierric11 <pierric.vernet@esme.fr>
2025-03-21 11:06:10 +0100

    script simu

--- simulator.js
+++ simulator.js
@@ -3,13 +3,14 @@ import { FlyControls } from 'three/addons/controls/FlyControls.js';
 
 const CAM_FOV = 60, CAM_NEAR = 0.1, CAM_FAR = 1000;
 const COLOR_SKY = 0x5dade2, COLOR_GROUND = 0x219313 , COLOR_LIGHT = 0xfdfefe;
+const ROLL_SPEED = 0.05;
 const view = document.getElementById('ecran');
 const camera = new THREE.PerspectiveCamera(CAM_FOV, view.clientWidth / view.clientHeight, CAM_NEAR, CAM_FAR);
 
 const scene = new THREE.Scene();
 const renderer = new THREE.WebGLRenderer({ canvas: view, antialias: true });
 const ground = new THREE.Mesh(
-    new THREE.PlaneGeometry( 10000, 10000 ),
+    new THREE.PlaneGeometry( 1000, 1000 ),
     new THREE.MeshStandardMaterial( { color: COLOR_GROUND } )
 );
 const light = new THREE.AmbientLight(COLOR_LIGHT);
@@ -36,7 +37,7 @@ renderer.setAnimationLoop(()=>{
     controls.update( delta );
     const pitch = 1.41;
     const roll = Math.PI/12;
-    back.style.transformation = 'translateY(pitch%) rotate(rollrad)';
-    cercle.style.transformation = 'rotate(rollrad)';
+    back.style.transformation = `translateY(${pitch}%) rotate(${roll}rad)`;
+    cercle.style.transformation = `rotate(${roll}rad)`;
     renderer.render( scene, camera );
 });
